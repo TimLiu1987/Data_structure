77a7528f73f2ffcdb99cd0010477b610b841e6c7
TimLiu1987 <59197061+TimLiu1987@users.noreply.github.com>
2021-11-14 15:18:35 +0800

    Create 使用堆疊進行Push和Pop.cpp

---
+++ 使用堆疊進行Push和Pop.cpp
@@ -0,0 +1,17 @@
+// 請寫一個Stack類別，裡面有建構子，Push(int x)、Pop()、PrintStack() 三個函式和 int capacity、int* array、int top 三個變數。capacity定義堆疊大小，array當作堆疊，top紀錄目前堆疊頂端的索引值，初始值設為-1表示堆疊為空。建構子將變數capacity、top設初始值，並建立長度為capacity的array，Push(int x)可將變數x加入到array堆疊頂端，Pop()可從array堆疊頂端取出一個元素，PrintStack()則列印出堆疊全部的元素內容(從由上而下Top-Down)。
+
+// 注意：預設程式中main函式請勿修改，僅可加上函式定義與函式原型。
+
+// 輸入範例：
+// 6 10 20 30 40 50 60 2
+
+// 輸出範例：
+// 40 30 20 10
+
+
+
+// 系統輸入：Push次數(最多10)+放入堆疊的元素+Pop次數
+
+// 結果輸出：函式PrintStack()的執行結果
+
+
